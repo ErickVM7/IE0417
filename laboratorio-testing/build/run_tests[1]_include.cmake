e437b5b1deff8d50d23be0b2d46c726be7ab58e9
ErickVM7 <erick01vm@gmail.com>
2026-05-30 14:17:14 -0600

    Eliminar build del repositorio y agregar gitignore

--- laboratorio-testing/build/run_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/erick/Desktop/IE0417/laboratorio-testing/build/run_tests[1]_tests.cmake")
-  include("/home/erick/Desktop/IE0417/laboratorio-testing/build/run_tests[1]_tests.cmake")
-else()
-  add_test(run_tests_NOT_BUILT run_tests_NOT_BUILT)
-endif()
